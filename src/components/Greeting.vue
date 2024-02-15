e11b9895fb543fe572da93455edeb240aad96f8b
joelhagg <93922208+Joelhagg@users.noreply.github.com>
2024-02-15 21:07:34 +0100

    First test lab made, basic test written

---
+++ src/components/Greeting.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      {{ greeting }}
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: "Greeting",
+  
+    data() {
+      return {
+        greeting: "Vue and TDD"
+      }
+    }
+  }
+  </script>
