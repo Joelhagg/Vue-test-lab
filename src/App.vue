03ba42d3d658ffeb2210059ab9b0b1f5690e3463
joelhagg <93922208+Joelhagg@users.noreply.github.com>
2024-02-16 21:08:02 +0100

    Add test for child components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Greeting msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Greeting from './components/Greeting.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Greeting
   }
 }
 </script>
--- tests/unit/Greetings.spec.js
+++ tests/unit/Greetings.spec.js
@@ -1,10 +1,37 @@
+/* eslint-disable vue/multi-word-component-names */
 import { mount } from "@vue/test-utils";
 import Greeting from "@/components/Greeting.vue";
+import Vue from "vue";
+import { shallowMount } from "@vue/test-utils";
 
 describe("Greeting.vue", () => {
   it("renders a greeting", () => {
     const wrapper = mount(Greeting);
 
     expect(wrapper.text()).toMatch("Vue and TDD");
+
+    const Child = Vue.component("Child", {
+      name: "Child",
+
+      template: "<div>Child component</div>",
+    });
+
+    const Parent = Vue.component("Parent", {
+      name: "Parent",
+
+      template: "<div><child /></div>",
+    });
+
+    const shallowWrapper = shallowMount(Child);
+    const mountWrapper = mount(Child);
+
+    console.log(shallowWrapper.html());
+    console.log(mountWrapper.html());
+
+    const shallowWrapper2 = shallowMount(Parent);
+    const mountWrapper2 = mount(Parent);
+
+    console.log(shallowWrapper2.html());
+    console.log(mountWrapper2.html());
   });
 });
